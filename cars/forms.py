f20e3272184ea99e9aee52da703e6438d9e6994e
unknown <leonardojpoletti@gmail.com>
2024-06-26 21:00:47 -0300

    Conexão do Formulario com o Banco

--- cars/forms.py
+++ cars/forms.py
@@ -1,5 +1,5 @@
 from django import forms
-from cars.models import Brand
+from cars.models import Brand, Car
 
 class CarsForm(forms.Form):
     model = forms.CharField(max_length=200)
@@ -9,3 +9,16 @@ class CarsForm(forms.Form):
     plate = forms.CharField(max_length=10)
     value = forms.FloatField()
     photos = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            value = self.cleaned_data['value'],
+            photos = self.cleaned_data['photos'],
+        )
+        car.save()
+        return car
